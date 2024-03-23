9e1bfba63a203c940d3f9bd99bea3bcfe8ea892b
Umang3211 <umangshankar10@gmail.com>
2024-03-23 11:05:46 -0400

    first commit

---
+++ webapp.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image 
+
+@st.cache_resource
+def mod():
+    model = YOLO("/Users/umangsharma/runs/classify/train2/weights/best.pt")
+    return model
+
+tab1, tab2 = st.tabs(["HOME","MYMODEL"])
+
+with tab1:
+    st.title("Umang")
+
+with tab2:
+    st.title("Prediction")
+    st.markdown("U[pload Image animals to check its class")
+    img = st.file_uploader("Upload an audio file", type=["jpg", "jpeg", "png"])
+    
+    if img is not None:
+        img = Image.open(img)
+        st.image(img)
+        model = mod()
+        res = model.predict(img)
+        label = res[0].probs.top5
+        conf = res[0].probs.top5conf
+        conf = conf.tolist()
+        col1,col2 = st.columns(2)
+        col1.subheader(res[0].names[label[0]].title() +' with '+ str(conf[0])+' Confidence')
+        col2.subheader(res[0].names[label[1]].title() +' with '+ str(conf[1])+' Confidence')
